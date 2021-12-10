56e8ebc5b01fada6d34586128654f89b685927e1
thetajwar2003 <rahmantajwar131@gmail.com>
2021-12-10 11:47:44 -0500

    built banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,22 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
 
       <Header />
+
+      <main className="mx-auto">
+        {/* Banner */}
+        <Banner />
+
+        {/* Product Feed */}
+      </main>
 
       <p></p>
     </div>
